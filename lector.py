3701da7bd8d3752b6fc94207057e60daa72ca102
Franco Duilmarc Chavez Ponce <franco.chp@gmail.com>
2018-05-11 08:00:28 -0500

    Update lector.py

--- lector.py
+++ lector.py
@@ -1,11 +1,66 @@
 
+class Grammar:
 
+    def __init__(self, produccion):
+        self.producciones=produccion
+
+
+class Produccion:
+    def __init__(self,lexema,lenma,clase_gramatical)
+        self.clase_gramatical=clase_gramatical
+
+    def motrar_producciones():
+        print("< "+lexema+" , "+lenma+" , "+clase_gramatical+" >")
+
+
+class lenma:
+    def __init__(self,conjunto_lexema,lenma)
+        self.conjunto_lexema[]=conjunto_lexema
+        self.nombre=lenma
+
+    def añadir(lexema):
+        conjunto_lexema=conjunto_lexema.append(lexema)
+
+    def imprimir():
+        print(self.nombre+" : ")
+        for i in conjunto_de_lenma
+            print(i+", ")
+    def buscar(lexema):
+        for i in conjunto_lexema:
+            if(i==lexema):
+                return 1
+            else:
+                return 0
+
+class clase_gramatical:
+    def __init__(self,conjunto_de_lenma,clase_gramatical)
+        self.conjunto_de_lenma=conjunto_de_lenma
+        self.nombre=clase_gramatical
+
+    def añadir(lenma):
+        conjunto_de_lenma=conjunto_de_lenma.append(lexema)
+    
+    def imprimir():
+        print(self.nombre+" : ")
+        for i in conjunto_de_lenma
+            print(i+", ")
+
+    def buscar(lexema):
+        for i in conjunto_lenma:
+            if(i==lexema):
+                return 1
+            else:
+                return 0
+
+
+def early_parser(gramatica,oracion)
 def comparar_articulos(palabra):
         for i in articulos:
              if(i==palabra):
                      return 1
 
         return 0
+
 def comparar_verbos(palabra):
         for i in verbos:
              if(i==palabra):
@@ -31,12 +86,12 @@ def leerarticu():
                 for palabra in linea.split(' '):
                         if(comparar_articulos(palabra)):
                                 contadorA+=1
-                                print (str(contadorA),palabra)
+                                print (str(contadorA)+')'+palabra)
                         if(comparar_verbos(palabra)):
                                 contadorV+=1
-                                print (str(contadorV),palabra)
+                                print (str(contadorV)+')'palabra)
                         if(comparar_sustantivos(palabra)):
                                 contadorS+=1
-                                print (str(contadorS),palabra)
+                                print (str(contadorS)+')'+palabra)
 
 leerarticu()
